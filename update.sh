3172a24386ccbe2a17a8910908b94293ef4cef09
Naoki Hirata <nhirata.bugzilla@gmail.com>
2016-04-05 18:06:05 -0700

    more scripts to build

--- update.sh
+++ update.sh
@@ -2,8 +2,13 @@
 
 . load-config.sh
 cd $GECKO_PATH
-hg pull
-hg up
+if [$GECKO_PATH~="pine"]||[$GECKO_PATH~="mozilla-inbound"]
+   hg pull
+   hg up
+else
+   git pull
+fi
+
 cd ..
 cd gaia
 git pull
